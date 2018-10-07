0920abe128e70444897297e38584a522b279214c
Alexey <lesha200111@gmail.com>
2018-10-07 12:30:42 +0300

    task1

---
+++ Bezmen/GeneratePassword.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int amountDigits = rand.Next(8, 24);
+            int randomSymbol;
+            string password = "";
+            string symbols = "zxcvbnmasdfghjklqwertyuiop123456789ZXCVBNMASDFGHJKLQWERTYUIOP";
+            for (int i = 0; i < amountDigits; i++)
+            {
+                randomSymbol = rand.Next(0, symbols.Length);
+                password += symbols[randomSymbol];
+            }
+            Console.WriteLine(password);
+            Console.ReadKey();
+        }
+    }
+}
